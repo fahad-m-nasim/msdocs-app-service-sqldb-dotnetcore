4ec5aee5b8d256365b5f8879366c9a2a211e2aa9
fahad-m-nasim <67058053+fahad-m-nasim@users.noreply.github.com>
2023-03-09 15:58:46 +1100

    Add project files.

---
+++ DotNetCoreSqlDb/Data/MyDatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreSqlDb.Models
+{
+    public class MyDatabaseContext : DbContext
+    {
+        public MyDatabaseContext (DbContextOptions<MyDatabaseContext> options)
+            : base(options)
+        {
+            //var conn = (System.Data.SqlClient.SqlConnection)Database.GetDbConnection;
+            //var credential = new Azure.Identity.DefaultAzureCredential();
+            //var token = credential.GetToken(new Azure.Core.TokenRequestContext(new[] { "https://database.windows.net/.default" }));
+            //conn.AccessToken = token.Token;
+        }
+
+        public DbSet<DotNetCoreSqlDb.Models.Todo> Todo { get; set; }
+    }
+}
